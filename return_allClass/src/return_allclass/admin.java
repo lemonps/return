b5668a9dd3d4a09bd2c13a98e3eda6a5922a23d8
DESKTOP-4733J6F\Lemon <pattharathon.srithundorn@mail.kmutt.ac.th>
2017-04-02 12:20:59 +0700

    ผมจะทำ method EditSubject  ใน class admin ครับพี่แต่ว่ามัน error ไม่รู้เป็นอะไร อยากให้พี่เฟิสช่วยดูให้หน่อยครับ

---
+++ return_allClass/src/return_allclass/admin.java
@@ -0,0 +1,58 @@
+package return_allclass;
+
+import java.sql.*;
+
+public class admin {
+
+    private String username;
+    private String password;
+    private int id;
+
+    public admin() {
+    }
+
+    public static void EditSubjectById(int id, String name, String co_id) throws SQLException, ClassNotFoundException, NullPointerException {
+        Connection con = ConnectionBuilder.getConnection();
+        Statement st = con.createStatement();
+        String sql = "update subject set name=?,course_id=? where id=?";
+        PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, name);
+            ps.setString(2, co_id);
+            ps.setInt(3, id);
+        ResultSet rs = ps.executeQuery(sql);
+            while (rs.next()) {
+                subject sub = new subject();
+                sub.setName(rs.getString("name"));
+                sub.setCourse_id(rs.getString("course_id"));
+            }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        admin.EditSubjectById(1, "a", "b");
+    }
+
+}
